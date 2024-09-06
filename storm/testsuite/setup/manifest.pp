a96dbb309148aad53e859b72a4a672a694bfec11
Enrico Vianello <enrico.vianello@cnaf.infn.it>
2024-09-06 21:44:03 +0200

    Update storm-testsuite image to Almalinux9
    Renamed to cnafsd/storm-testsuite:el9

--- storm/testsuite/setup/manifest.pp
+++ storm/testsuite/setup/manifest.pp
@@ -1,50 +1,50 @@
 include epel
-include umd4
 include testca
-include sdds_users
+include sdds_users::sd_users
+include umd::umd5
 
 include voms::dteam
 include voms::testvo
 include voms::testvo2
+include voms::escape
+include voms::ops
+include voms::wlcg
+include voms::cms
+include voms::atlas
 
-file { '/etc/grid-security':
-  ensure => directory,
+$carepo = 'https://repository.egi.eu/sw/production/cas/1/current/'
+$carepo_gpgkey = 'https://dist.eugridpma.info/distribution/igtf/current/GPG-KEY-EUGridPMA-RPM-3'
+
+yumrepo { 'carepo':
+  descr    => 'IGTF CA Repository',
+  enabled  => 1,
+  baseurl  => $carepo,
+  gpgcheck => 1,
+  gpgkey   => $carepo_gpgkey,
 }
 
-class { 'storm::repo':
-  enabled => ['stable'],
+class { 'voms::repo':
+  enabled => ['stable', 'beta'],
 }
 
 $packages = [
+  'ca-policy-egi-core',
   'openldap-clients',
   'globus-gass-copy-progs',
-  'gfal2-util',
   'gfal2-all',
-  'storm-srm-client',
+  'python3-gfal2-util',
   'davix',
   'myproxy',
   'voms-clients',
   'voms-clients-java',
 ]
 
-package { 'dcache-srmclient-3.0.9-1':
-  ensure  => 'installed',
-  require => [Class['umd4']],
-}
-
 package { $packages:
-  ensure  => 'latest',
-  require => [Class['umd4']],
+  ensure => 'latest',
 }
 
-yumrepo { 'voms-0621-01':
-  ensure   => present,
-  descr    => 'voms unreleased rpms',
-  baseurl  => 'https://ci.cloud.cnaf.infn.it/view/voms/job/pkg.voms/job/v0621.01/lastSuccessfulBuild/artifact/artifacts/stage-area/centos7',
-  enabled  => 1,
-  protect  => 1,
-  priority => 1,
-  gpgcheck => 0,
+class { 'java' :
+  package => 'java-1.8.0-openjdk-devel',
 }
 
 class { 'python':
@@ -84,10 +84,6 @@ package { 'shyaml':
   provider => 'pip3',
 }
 
-class { 'java' :
-  package => 'java-1.8.0-openjdk-devel',
-}
-
 user { 'tester':
   ensure     => present,
   name       => $title,
@@ -102,16 +98,17 @@ sudo::conf { 'tester':
 }
 
 Class['epel']
--> Class['umd4']
--> Class['python']
+-> Class['umd::umd5']
+-> Class['voms::repo']
+-> Yumrepo['carepo']
 -> Class['java']
--> Class['sdds_users']
--> File['/etc/grid-security']
+-> Class['python']
+-> Class['sdds_users::sd_users']
+-> Class['testca']
 -> Class['voms::dteam']
 -> Class['voms::testvo']
 -> Class['voms::testvo2']
--> Class['testca']
--> Class['storm::repo']
--> Yumrepo['voms-0621-01']
--> Package['robotframework']
+-> Class['voms::ops']
+-> Class['voms::wlcg']
+-> Package[$packages]
 -> User['tester']
